e8445617cb8521d2700cf1032d6aa89cc282ef2b
Nozomi04k <140169140+Nozomi04k@users.noreply.github.com>
2023-11-09 13:29:33 +0900

    公開

---
+++ works/js/script.js
@@ -0,0 +1,67 @@
+(function (d) {
+    var config = {
+        kitId: 'ztc4geb',
+        scriptTimeout: 3000,
+        async: true
+    },
+        h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
+})(document);
+
+$(function () {
+    // マウスホイールを横スクロールにする
+    const scrollElement = document.querySelector("main");
+
+    scrollElement.addEventListener("wheel", (e) => {
+        if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+
+        const maxScrollLeft = scrollElement.scrollWidth - scrollElement.clientWidth;
+
+        if (
+            (scrollElement.scrollLeft <= 0 && e.deltaY < 0) ||
+            (scrollElement.scrollLeft >= maxScrollLeft && e.deltaY > 0)
+        )
+            return;
+
+        e.preventDefault();
+        scrollElement.scrollLeft += e.deltaY;
+    });
+
+
+        // $('.works-slider').slick({
+        //   slidesToScroll: 1,
+        //   slidesToShow: 3,
+        // });
+        // var slidesToScroll = 1;
+        // $('.js-setoption').on('click', function() {
+        //   if (slidesToScroll === 1) {
+        //     $(this).text('1つに戻す');
+        //     slidesToScroll = 3;
+        //   } else {
+        //     $(this).text('3つにする');
+        //     slidesToScroll = 1;
+        //   }
+        //   $('.slider').slick('slickSetOption', {
+        //     slidesToScroll: slidesToScroll,
+        //   });
+        // });
+    
+      
+    $('.works-slider').slick({
+        autoplay: false,
+        dots: false,
+        arrows: true,
+        Infinity: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+
+                }
+            },
+        ]
+    });
+});
